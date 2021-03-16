229d46694dd0c1dc270486c8465a6f085e9d4927
青野凌介 <piesuke0727@gmail.com>
2021-03-16 16:53:05 +0900

    軽微な変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,13 +42,14 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
-  routes: [
-    "/articles/baton",
-    "/articles/evangelion",
-    "/articles/sato_kashiwa",
-    "/about"
-  ],
+  generate: {
+    routes: [
+      "/articles/baton",
+      "/articles/evangelion",
+      "/articles/sato_kashiwa",
+      "/about"
+    ]
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
